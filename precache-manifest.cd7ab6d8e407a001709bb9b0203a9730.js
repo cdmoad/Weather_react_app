3ba5f9021fc5353391870e1f681dc575dfa7d033
cdmoad <moadehrimech22@gmail.com>
2023-02-26 16:57:40 +0100

    Updates

---
+++ precache-manifest.cd7ab6d8e407a001709bb9b0203a9730.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f5ab5bde95201ae7223",
+    "url": "/Weather_react_app.git/static/css/main.aecf5617.chunk.css"
+  },
+  {
+    "revision": "1f5ab5bde95201ae7223",
+    "url": "/Weather_react_app.git/static/js/main.1f5ab5bd.chunk.js"
+  },
+  {
+    "revision": "71cc9df0fcc57d0ce516",
+    "url": "/Weather_react_app.git/static/js/1.71cc9df0.chunk.js"
+  },
+  {
+    "revision": "5957a9fdf8a3e38bb500",
+    "url": "/Weather_react_app.git/static/js/2.5957a9fd.chunk.js"
+  },
+  {
+    "revision": "0f0d5a7c4bd18619d19b",
+    "url": "/Weather_react_app.git/static/js/runtime~main.0f0d5a7c.js"
+  },
+  {
+    "revision": "d948be303f11fe807d7c79f5860936f2",
+    "url": "/Weather_react_app.git/static/media/sunny.d948be30.jpg"
+  },
+  {
+    "revision": "9a9ac0618cb197d5b2b21c95ba0c7846",
+    "url": "/Weather_react_app.git/static/media/cloudy.9a9ac061.jpg"
+  },
+  {
+    "revision": "1578635b379b74bee1d3c49647b6b346",
+    "url": "/Weather_react_app.git/static/media/bg2.1578635b.jpg"
+  },
+  {
+    "revision": "99bbaaa9bf88f67396e5b11ed4cfa860",
+    "url": "/Weather_react_app.git/static/media/rainy.99bbaaa9.jpg"
+  },
+  {
+    "revision": "187eb9746364305ff4ef9fc28992cd9a",
+    "url": "/Weather_react_app.git/static/media/temp.187eb974.jpg"
+  },
+  {
+    "revision": "a0b1d6196f19813d41f7a6ad5e69bef2",
+    "url": "/Weather_react_app.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Weather_react_app.git/precache-manifest.cd7ab6d8e407a001709bb9b0203a9730.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Weather_react_app.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
